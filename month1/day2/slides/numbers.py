eb3dc2a64ea61c92031bc7dbf7f3c6cc78e393ae
adilkaaa <adilkhanakakov@gmail.com>
2022-03-15 19:24:45 +0600

    add numbers.py

---
+++ month1/day2/slides/numbers.py
@@ -0,0 +1,80 @@
+#1
+num = 17925
+n1 =  34**2
+n2 = 26*3
+n3 = 17*33
+n4 = 4394*4
+if n1>=num:
+	print(n1)
+if n2 >=num:
+	print(n2)
+if n3>=num:
+	print(n3)
+if n4>=num:
+	print(n4)
+print('1)______')
+#2
+a = 7
+b = 4
+c = 8
+s1 = b<a<c
+s2 = 7%3 * 4.8
+s3 = ((a)==(b+3))
+s4 = ((9+2)!= (8+1))
+print('2)',s1,s2,s3,s4)
+
+#3
+a = (12+23)
+b = a*3
+c = b**2
+d = c-193432
+print('3)',d)
+#4
+a = 17*3
+b = 12*5
+c = 12**3
+d =13*7
+e = 4**5
+f = 512+512
+print('4)',a,b,c,d,e,f,'biggest (1728)')
+
+
+#5
+a = -21//10
+print('5)',a, 'to less side')
+
+#6
+a = 22
+b =21
+formula = (a/b)*100
+print('6)',formula)
+
+#7
+year = 2005
+const = 2022
+after2 = const-year+2
+before2 = const-year-2
+print('7)',after2,before2)
+print()
+
+#8
+print('8)',(25+75+10+95)/4)
+
+#9
+n1 = 2.5
+n2 = 2.5
+n3 = 2.5
+n = (n1+n2)*n3
+print('9)',n)
+
+
+#10
+a = 2
+b = 7
+c = 9.7
+d = 3
+e = 1
+print('10)_______')
+#11
+s = (a-e**(b/d))%c
+print('11)',s)
